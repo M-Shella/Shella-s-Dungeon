692c2eebb0284a257f1453e7202e320e03471155
M-Shella <matejsela@gmail.com>
2021-06-12 19:54:19 +0200

    neco

---
+++ Assets/Skripty/Player, Enemy/UtokNaDalku.cs
@@ -0,0 +1,23 @@
+﻿using Pathfinding;
+using UnityEngine;
+
+public class UtokNaDalku : MonoBehaviour {
+    public Transform PoziceHrace;
+    public Hrac hrac;
+    public Enemy enemy;
+    private float _pomocna;
+    public AIPath aipath;
+    public GameObject strela;
+    private void Start() {
+        PoziceHrace = GameObject.FindGameObjectWithTag("Hrac").GetComponent<Transform>();
+    }   
+    private void FixedUpdate() {
+        if (aipath.velocity == Vector3.zero && Vector2.Distance(transform.position , PoziceHrace.position) < 8) {
+            if (Time.time > _pomocna) {
+                var spawn = Instantiate(strela, transform.position, Quaternion.identity);
+                spawn.GetComponent<Strela>().enemy = enemy;
+                _pomocna = Time.time + 5f;
+            }
+        }
+    }
+}
